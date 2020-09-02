03de3bddcdb8be85b5aafcfa7d1e0767938f7ad0
Troy Sargent <troyjsargent@gmail.com>
2020-09-02 10:14:51 -0500

    converting from html to handle bars

--- controllers/authenticationroutes.js
+++ controllers/authenticationroutes.js
@@ -8,23 +8,23 @@ module.exports = function (app) {
   app.get('/', (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect('/members');
+      res.render('landing');
     }
-    res.sendFile(path.join(__dirname, '../public/signup.html'));
+    res.render('signup', { layout: false });
   });
 
   app.get('/login', (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect('/members');
+      res.render('landing');
     }
-    res.sendFile(path.join(__dirname, '../public/login.html'));
+    res.render('login', { layout: false });
   });
 
   /* Here we've add our isAuthenticated middleware to this route.
   If a user who is not logged in tries to access
   this route they will be redirected to the signup page */
   app.get('/members', isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/members.html'));
+    res.render('landing');
   });
 };
