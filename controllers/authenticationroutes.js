ab59362c3d542994690e60b8fd006cc82841baea
Troy Sargent <troyjsargent@gmail.com>
2020-09-02 11:36:59 -0500

    adding page routing to expenses and income

--- controllers/authenticationroutes.js
+++ controllers/authenticationroutes.js
@@ -21,7 +21,15 @@ module.exports = function (app) {
   /* Here we've add our isAuthenticated middleware to this route.
   If a user who is not logged in tries to access
   this route they will be redirected to the signup page */
-  app.get('/members', isAuthenticated, (req, res) => {
+  app.get('/home', isAuthenticated, (req, res) => {
     res.render('landing');
+  });
+
+  app.get('/expenses', isAuthenticated, (req, res) => {
+    res.render('expenses');
+  });
+
+  app.get('/income', isAuthenticated, (req, res) => {
+    res.render('income');
   });
 };
