a9c7f112c8e6b05ab1f339a15d30de9f66cf8f1d
Troy Sargent <troyjsargent@gmail.com>
2020-09-03 13:23:24 -0500

    plus and minus button added and styled

--- public/js/home.js
+++ public/js/home.js
@@ -26,12 +26,7 @@ fetch('/api/expenses').then((response) => response.json()).then((data) => {
   const expenseData = data[0].ExpenseCategories;
   expenseData.forEach((category) => {
     const categorySection = createNode('ul');
-    const expenseItem = createNode('li');
-    for (let i = 0; i < 4; i += 1) {
       categorySection.innerHTML = `${category.categoryName}`;
       append(expenseSection, categorySection);
-      categorySection.appendChild(expenseItem);
-      expenseItem.textContent = `${category.Expenses[i].expenseName}, $${category.Expenses[i].total}`;
-    }
   });
 }).catch((err) => err);
