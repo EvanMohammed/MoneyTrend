dffa186cca33145f2f04c8fe951d0cce986ae557
Evan <evanmohammedsaleh@gmail.com>
2020-09-03 11:48:34 -0500

    incomeTotalAdded

--- controllers/incomecontroller.js
+++ controllers/incomecontroller.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { QueryTypes } = require('sequelize');
 
 const router = express.Router();
 
@@ -7,11 +8,27 @@ const db = require('../models');
 router.get('/api/income', (req, res) => {
   db.User.findAll({
     where: {
-      id: 1,
+      UserId: 2,
     },
     include: db.Incomes,
   }).then((income) => {
     res.json(income);
+  });
+});
+
+router.get('/api/income/total', (req, res) => {
+  db.sequelize.query(
+    `SELECT SUM(total) FROM incomes
+    inner join users on incomes.UserId = users.id
+   WHERE UserId = ?
+    ;`,
+    {
+      replacements: [req.user.id],
+      type: QueryTypes.SELECT,
+    },
+  ).then((data) => {
+    console.log(data)
+    res.json(data);
   });
 });
 
